9ca24fb02c27876f68a8c2818ce0ebbc228a30ee
zArjun <mallikarjun.somanakatti@gmail.com>
2023-03-04 14:41:38 +0530

    Read selected strings from a file

---
+++ read_string_from_file.py
@@ -0,0 +1,14 @@
+def create_cast_list(filename):
+    cast_list = []
+    #use with to open the file filename
+    with open (filename) as f:
+        for line in f:
+            cast_list.append(line.split(',')[0])
+    #use the for loop syntax to process each line
+    #and add the actor name to cast_list
+
+    return cast_list
+
+c_list = create_cast_list('../python-programming/flying_circus_cast.txt')
+for actor in c_list:
+    print(actor)
